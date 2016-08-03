33cfb5454ae679c68298d6c68fb665674b5c50be
Pearl Li <pearlzli16@gmail.com>
2016-08-03 11:04:16 -0400

    Revert "start implementing forcing process in kalman filter"
    This reverts commit 3fb7f40948488f28c23e8049e3fdacc9586453e1.

--- src/models/m990/measurement.jl
+++ src/models/m990/measurement.jl
@@ -157,10 +157,6 @@ type Measurement{T<:AbstractFloat}
     EE::Matrix{T}
     MM::Matrix{T}
     VVall::Matrix{T}
-    A::Matrix{T}
-end
-function Measurement{T<:AbstractFloat}(ZZ, DD, QQ, EE, MM, VVall)
-    Measurement(ZZ, DD, QQ, EE, MM, VVall, [])
 end
 function Base.getindex(M::Measurement, d::Symbol)
     if d in (:ZZ, :DD, :QQ, :EE, :MM, :VVall)
