4d7ea2b5868a11df5e3a22f607176effae1606ea
Pearl Li <pearlzli16@gmail.com>
2016-10-12 11:38:19 -0400

    Fix Kalman concatenation bug and non-forecast tests

--- src/estimate/kalman.jl
+++ src/estimate/kalman.jl
@@ -299,7 +299,7 @@ function kalman_filter_2part{S<:AbstractFloat}(m::AbstractModel,
                                                allout::Bool   = false,
                                                catch_errors::Bool = false,
                                                include_presample::Bool = true)
-    
+
     # Partition sample into three regimes, and store associated matrices:
     # - R1: presample
     # - R2: normal
@@ -349,7 +349,7 @@ function kalman_filter_2part{S<:AbstractFloat}(m::AbstractModel,
         R3[:TTT], R3[:RRR], R3[:CCC] = TTT, RRR, CCC
     end
 
-    
+
     # Get normal, no ZLB matrices
     R2[:TTT] = R3[:TTT][state_inds, state_inds]
     R2[:RRR] = R3[:RRR][state_inds, shock_inds]
@@ -500,10 +500,10 @@ immutable Kalman{S<:AbstractFloat}
     Pend::Matrix{S}       # last-period variance-covariance matrix for the states
     pred::Matrix{S}       # predicted value of states in period T+1
     vpred::Array{S, 3}    # predicted variance-covariance matrix for states in period T+1
-    yprederror::Matrix{S} 
+    yprederror::Matrix{S}
     ystdprederror::Matrix{S}
     rmse::Matrix{S}
-    rmsd::Matrix{S}        # 
+    rmsd::Matrix{S}        #
     filt::Matrix{S}        # filtered states
     vfilt::Array{S, 3}     # mean square errors of filtered state vectors
     z0::Vector{S}          # starting-period state vector
