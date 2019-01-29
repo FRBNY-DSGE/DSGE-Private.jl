9efa3562a5c6051562caa8405e040fcb80c3cc2b
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-01-29 14:01:25 -0500

    Most recent tolerances for testing

--- src/estimate/posterior.jl
+++ src/estimate/posterior.jl
@@ -42,11 +42,7 @@ function posterior{T<:AbstractFloat}(m::AbstractModel{T},
     catch_errors = catch_errors | sampler
     like = likelihood(m, data; sampler=sampler, catch_errors=catch_errors)
     post = ϕ_smc*like + prior(m)
-    if sampler
-        return post
-    else
-        return post
-    end
+    return post
 end
 
 """
