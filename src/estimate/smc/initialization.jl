519be25206b5b5a7addc3af6da08650a627a2aa5
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-01-29 15:54:39 -0500

    Remove dependency on reading from FORTRAN output.

--- src/estimate/smc/initialization.jl
+++ src/estimate/smc/initialization.jl
@@ -1,109 +1,5 @@
 """
 ```
-initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud, readin::Matrix{Float64})
-```
-
-Draw from a general starting distribution (set by default to be from the prior) to initialize the SMC algorithm.
-Returns a tuple (logpost, loglh) and modifies the particle objects in the particle cloud in place.
-
-"""
-function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud,
-                       path::String; # RECA
-                       parallel::Bool = false, use_chand_recursion::Bool = true,
-                       verbose::Symbol = :low)
-    # legacy - not currently necessary
-    n_parts = length(c)
-    loglh = zeros(n_parts)
-    logpost = zeros(n_parts)
-#=
-    draws   = transpose(readdlm(path * "001parasim.txt"))
-    if parallel
-        draws, loglh, logpost = @sync @parallel (vector_reduce) for i in 1:n_parts
-            draw = vec(draws[i]) #vec(rand(m.parameters, 1))
-            draw_loglh = 0.
-            draw_logpost = 0.
-            success = false
-            while !success
-                try
-                    update!(m, draw)
-                    draw_loglh   = likelihood(m, data, catch_errors = true, use_chand_recursion=use_chand_recursion, verbose = verbose)
-                    draw_logpost = prior(m)
-                    if (draw_loglh == -Inf) | (draw_loglh===NaN)
-                        draw_logpost = -Inf
-                        draw_loglh = -Inf
-                    end
-                catch err
-                    if isa(err, ParamBoundsError)
-                        draw_loglh = draw_logpost = -Inf
-                        #elseif isa(err, SPDError)
-                        #    draw_loglh = draw_logpost = -Inf
-                    else
-                        #   draw_loglh = draw_logpost = -Inf
-                        throw(err)
-                    end
-                end
-
-                if isinf(draw_loglh)
-                    draw = vec(rand(m.parameters, 1))
-                else
-                    success = true
-                end
-            end
-            vector_reshape(draw, draw_loglh, draw_logpost)
-        end
-        loglh = squeeze(loglh, 1)
-        logpost = squeeze(logpost, 1)
-    else
-        #draws = rand(m.parameters, n_parts)
-        for i in 1:n_parts
-            success = false
-            while !success
-                try
-                    update!(m, draws[:, i])
-                    loglh[i] = likelihood(m, data, catch_errors = true, use_chand_recursion = use_chand_recursion, verbose = verbose)
-                    logpost[i] = prior(m)
-                    if (loglh[i] == -Inf) | (loglh[i]===NaN)
-                        logpost[i] = -Inf
-                        loglh[i] = -Inf
-                    end
-                catch err
-                    if isa(err, ParamBoundsError)
-                        loglh[i] = logpost[i] = -Inf #draws[:, i] = rand(m.parameters, 1)
-                        #continue
-                    else
-                        throw(err)
-                    end
-                end
-                if isinf(loglh[i])
-                    @show "REDRAW AT PARTICLE", i
-                    draws[:, i] = rand(m.parameters, 1)
-                else
-                    success = true
-                end
-            end
-        end
-    end
-
-    draws_orig   = transpose(readdlm(path * "001parasim.txt"))
-    loglh_orig   = vec(readdlm(path * "001liksim.txt"))
-    logpost_orig = vec(readdlm(path * "001postsim.txt"))
-
-    @test_matrix_approx_eq draws_orig draws
-    @test_matrix_approx_eq loglh_orig loglh
-    @test_matrix_approx_eq logpost_orig logpost
-=#
-    draws   = transpose(readdlm(path * "001parasim.txt"))
-    loglh   = vec(readdlm(path * "001liksim.txt"))
-    logpost = vec(readdlm(path * "001postsim.txt")) - loglh
-    # redraw at 11371
-
-    update_draws!(c, draws)
-    update_loglh!(c, loglh)
-    update_logpost!(c, logpost)
-end
-
-"""
-```
 initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud)
 ```
 
@@ -114,18 +10,19 @@ Returns a tuple (logpost, loglh) and modifies the particle objects in the partic
 function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud;
                        parallel::Bool = false, use_chand_recursion::Bool = true, verbose::Symbol = :low)
     n_parts = length(c)
-    loglh = zeros(n_parts)
+    loglh   = zeros(n_parts)
     logpost = zeros(n_parts)
     if parallel
         draws, loglh, logpost = @sync @parallel (vector_reduce) for i in 1:n_parts
-            draw = vec(rand(m.parameters, 1))
-            draw_loglh = 0.
+            draw         = vec(rand(m.parameters, 1))
+            draw_loglh   = 0.
             draw_logpost = 0.
-            success = false
+            success      = false
             while !success
                 try
                     update!(m, draw)
-                    draw_loglh   = likelihood(m, data, catch_errors = true, use_chand_recursion=use_chand_recursion, verbose = verbose)
+                    draw_loglh = likelihood(m, data, catch_errors = true,
+                                            use_chand_recursion=use_chand_recursion, verbose = verbose)
                     draw_logpost = prior(m)
                     if (draw_loglh == -Inf) | (draw_loglh===NaN)
                         draw_logpost = -Inf
@@ -159,7 +56,8 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
             while !success
                 try
                     update!(m, draws[:, i])
-                    loglh[i] = likelihood(m, data, catch_errors = true, use_chand_recursion = use_chand_recursion, verbose = verbose)
+                    loglh[i] = likelihood(m, data, catch_errors = true,
+                                          use_chand_recursion = use_chand_recursion, verbose = verbose)
                     logpost[i] = prior(m)
                     if (loglh[i] == -Inf) | (loglh[i]===NaN)
                         logpost[i] = -Inf
@@ -181,16 +79,21 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
             end
         end
     end
-
     update_draws!(c, draws)
     update_loglh!(c, loglh)
     update_logpost!(c, logpost)
 end
 
-# This function is made for transfering the log-likelihood values saved in the
-# ParticleCloud from a previous estimation to each particle's respective old_loglh
-# field, and for evaluating/saving the likelihood and posterior at the new data, which
-# here is just the argument, data.
+"""
+```
+initialize_likelihoods!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud;
+                        parallel::Bool = false, verbose::Symbol = :low)
+```
+This function is made for transfering the log-likelihood values saved in the
+ParticleCloud from a previous estimation to each particle's respective old_loglh
+field, and for evaluating/saving the likelihood and posterior at the new data, which
+here is just the argument, data.
+"""
 function initialize_likelihoods!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud;
                                  parallel::Bool = false, verbose::Symbol = :low)
     # Retire log-likelihood values from the old estimation to the field old_loglh
@@ -227,16 +130,18 @@ function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud; temp
     n_parts = length(cloud)
 
     cloud.tempering_schedule = zeros(1)
+
     if tempered_update
         cloud.ESS = [cloud.ESS[end]]
     else
         cloud.ESS[1] = n_parts
     end
+
     cloud.stage_index = 1
-    cloud.n_Φ = get_setting(m, :n_Φ)
-    cloud.resamples = 0
-    cloud.c = get_setting(m, :step_size_smc)
-    cloud.accept = get_setting(m, :target_accept)
+    cloud.n_Φ         = get_setting(m, :n_Φ)
+    cloud.resamples   = 0
+    cloud.c           = get_setting(m, :step_size_smc)
+    cloud.accept      = get_setting(m, :target_accept)
     cloud.total_sampling_time = 0.
 
     return nothing
