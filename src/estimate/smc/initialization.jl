318892ef631b135b872beaa462ac4da60795e270
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-02-05 10:59:58 -0500

    Add unit tests for helper methods, provide docstrings for functions, add test files.

--- src/estimate/smc/initialization.jl
+++ src/estimate/smc/initialization.jl
@@ -3,12 +3,13 @@
 initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud)
 ```
 
-Draw from a general starting distribution (set by default to be from the prior) to initialize the SMC algorithm.
-Returns a tuple (logpost, loglh) and modifies the particle objects in the particle cloud in place.
-
+Draw from a general starting distribution (set by default to be from the prior) to
+initialize the SMC algorithm. Returns a tuple (logpost, loglh) and modifies the
+particle objects in the particle cloud in place.
 """
 function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud;
-                       parallel::Bool = false, use_chand_recursion::Bool = true, verbose::Symbol = :low)
+                       parallel::Bool = false, use_chand_recursion::Bool = true,
+                       verbose::Symbol = :low)
     n_parts = length(c)
     loglh   = zeros(n_parts)
     logpost = zeros(n_parts)
@@ -22,7 +23,8 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                 try
                     update!(m, draw)
                     draw_loglh = likelihood(m, data, catch_errors = true,
-                                            use_chand_recursion=use_chand_recursion, verbose = verbose)
+                                            use_chand_recursion=use_chand_recursion,
+                                            verbose = verbose)
                     draw_logpost = prior(m)
                     if (draw_loglh == -Inf) | (draw_loglh===NaN)
                         draw_logpost = -Inf
@@ -31,10 +33,7 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                 catch err
                     if isa(err, ParamBoundsError)
                         draw_loglh = draw_logpost = -Inf
-                    #elseif isa(err, SPDError)
-                    #    draw_loglh = draw_logpost = -Inf
                     else
-                     #   draw_loglh = draw_logpost = -Inf
                         throw(err)
                     end
                 end
@@ -65,8 +64,7 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                     end
                 catch err
                     if isa(err, ParamBoundsError)
-                        loglh[i] = logpost[i] = -Inf #draws[:, i] = rand(m.parameters, 1)
-                        #continue
+                        loglh[i] = logpost[i] = -Inf
                     else
                         throw(err)
                     end
@@ -126,9 +124,16 @@ function initialize_likelihoods!(m::AbstractModel, data::Matrix{Float64}, c::Par
     update_logpost!(c, logpost)
 end
 
-function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud; tempered_update::Bool = false)
+"""
+```
+function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud;
+                                    tempered_update::Bool = false)
+```
+Initializes stage index, number of Φ stages, c, resamples, acceptance, and sampling time.
+"""
+function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud;
+                                    tempered_update::Bool = false)
     n_parts = length(cloud)
-
     cloud.tempering_schedule = zeros(1)
 
     if tempered_update
