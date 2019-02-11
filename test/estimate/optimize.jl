5e9435f988ffe5f2002c4462901939bd98ac86b9
Ethan Matlin <ethanmatlin@gmail.com>
2019-02-11 16:56:42 -0500

    Cherrypick 8eecb6c from dev squashing '1+' bug in AS

--- test/estimate/optimize.jl
+++ test/estimate/optimize.jl
@@ -9,6 +9,10 @@ m = AnSchorfheide(custom_settings = custom_settings, testing = true)
 
 
 file = "$path/../reference/optimize.h5"
+# For regenerating test file
+#=params_test = h5read(file, "params")
+data_test = h5read(file, "data")=#
+
 x0 = h5read(file, "params")
 data = h5read(file, "data")'
 minimizer = h5read(file, "minimizer")
@@ -22,11 +26,19 @@ n_iterations = 3
 x0 = Float64[p.value for p in m.parameters]
 
 out, H = optimize!(m, data; iterations=n_iterations)
-@testset "check optimize minimizers are the same" begin
+
+# Re-generate test file
+#= h5open("$path/../reference/optimize.h5", "w") do file
+    file["params"] = params_test
+    file["data"] = data_test
+    file["minimizer"] = out.minimizer
+    file["minimum"] = out.minimum
+    file["H"] = H
+end =#
+
+@testset "Check optimize minimizers are the same" begin
     @test @test_matrix_approx_eq minimizer out.minimizer
-    @show minimum
-    @show out.minimum
-    @test minimum ≈ out.minimum atol=5e-3
+    @test minimum ≈ out.minimum atol=5e-7
     @test @test_matrix_approx_eq H_expected H
 end
 
