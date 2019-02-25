82d2738e6792b0d8b5ec0483879f457b3a3fcc09
Michael Cai <michaeldcai@gmail.com>
2019-02-25 14:23:16 -0500

    Don't print adaptive tempering target

--- src/defaults.jl
+++ src/defaults.jl
@@ -151,7 +151,7 @@ function default_settings!(m::AbstractModel)
     settings[:tempering_target] = Setting(:tempering_target, 0.95, "The coefficient of the sample size metric to be targeted when solving for an endogenous ϕ")
     settings[:resampling_threshold] = Setting(:resampling_threshold, 0.5, "The threshold such that the particles will be resampled when the population drops below threshold * N")
     # temporary setting to save different output files
-    settings[:adaptive_tempering_target_smc] = Setting(:adaptive_tempering_target_smc, 0.97, true, "adpt", "Either the adaptive tempering target or 0.0 if using fixed schedule")
+    settings[:adaptive_tempering_target_smc] = Setting(:adaptive_tempering_target_smc, 0.97, false, "adpt", "Either the adaptive tempering target or 0.0 if using fixed schedule")
     settings[:previous_data_vintage] = Setting(:previous_data_vintage, vint, "The old data vintage to start SMC from when time tempering.")
     # Alternative policy
     baseline_policy = AltPolicy(:historical, eqcond, solve, forecast_init = identity)
