50cc9929fcf0914d551cf7bfc57c13e74324a3f1
Nathaniel Hickok <nghickok@gmail.com>
2019-07-03 09:32:09 -0400

    Changed solve to reflect new approx object

--- src/solve/solve.jl
+++ src/solve/solve.jl
@@ -63,28 +63,23 @@ function solve(m::GHLS)
     Γ0, Γ1, C, Ψ, Π  = eqcond(m)
 
     #SIMULATE LINEAR HERE
-    endog_emean, zlbfrequency, msvbounds, statezlbinfo, convergence = simulate_linear(m, m.approx)
-    m.approx <= Setting(:endog_emean, endog_emean)
-    m.approx <= Setting(:zlbfrequency, zlbfrequency)
-    m.approx <= Setting(:msvbounds, msvbounds)
-    m.approx <= Setting(:statezlbinfo, statezlbinfo)
-    m.approx <= Setting(:convergence, convergence)
+    m.approx.endog_emean, m.approx.zlbfrequency, m.approx.msvbounds, m.approx.statezlbinfo, m.approx.convergence = simulate_linear(m, m.approx)
 
     #Total number of state variables - these values should come from model
-    nmsvplus = m.approx[:nmsv] + m.approx[:nexogcont]
+    nmsvplus = m.approx.nmsv + m.approx.nexogcont
 
     # Conversion from state domain (msv) to [-1, 1] domain (xx)
     slopeconmsv = zeros(2*nmsvplus)
     slopeconmsv[1:nmsvplus] = 2.0 ./  (msvbounds[nmsvplus + 1 :  2*nmsvpluss] - msvbounds[1:nmsvplus])
     slopeconmsv[nmsvplus+1:2*nmsvplus] = -2.0 * msvbounds[1:nmsvplus] ./ (msvbounds[nmsvplus + 1 : 2*nmsvplus] .- msvbounds[1:nmsvplus]) .- 1.
-    m.approx <= Setting(:slopeconmsv, slopeconmsv)
+    m.approx.slopeconmsv = slopeconmsv)
 
     # Conversion from xx to msv domains
     slopeconxx = zeros(2*nmsvplus)
     slopeconxx[1:nmsvplus] =  0.5 * (msvbounds[nmsvplus + 1 : 2*nmsvplus] - msvbounds[1:nmsvplus])
     slopeconxx[nmsvplus+1:2*nmsvplus] = msvbounds[1:nmsvplus] + 0.5 * (msvbounds[nmsvplus + 1 : 2*nmsvplus] - msvbounds[1:nmsvplus])
 
-    m.approx <= Setting(:slopeconxx, slopeconxx)
+    m.approx.slopeconxx = slopeconxx
 
     # Construct starting guess
     aalin, bblin = lindecrule_markov(Γ0, Γ1, C, Ψ, Π)
@@ -105,8 +100,8 @@ function fixedpoint(m::GHLS, approx::SmolyakApproximation, α_initial::Array{Flo
 
     # Initialize
     α_star = copy(α_initial)
-    α_new = Array{Float64}(undef, approx[:nfunc]*approx[:ngrid], 2*approx[:ns])
-    α_temp = Array{Float64}(undef, 2*approx[:nfunc], approx[:ngrid])
+    α_new = Array{Float64}(undef, approx.nfunc*approx.ngrid, 2*approx.ns)
+    α_temp = Array{Float64}(undef, 2*approx.nfunc, approx.ngrid)
     convergence = false
 
     # Settings - should probably be stored in model
@@ -118,22 +113,22 @@ function fixedpoint(m::GHLS, approx::SmolyakApproximation, α_initial::Array{Flo
     for i in 1:niter
         avg_error = 0.0
 
-        for j in 1:approx[:ns]
+        for j in 1:approx.ns
 
-            updated_approx_polynomials = zeros(2*approx[:nfunc], approx[:ngrid])
+            updated_approx_polynomials = zeros(2*approx.nfunc, approx.ngrid)
             err = 0.0
 
             # Update polynomials using new guess for α
-            for k in 1:approx[:ngrid]
+            for k in 1:approx.ngrid
                 updated_approx_polynomials[:, k], err = decr_euler(k, j, m, approx, α_star)
             end
 
             # Solve for α by multiplying by inverse matrix and then reindex
-            for k in 1:approx[:ngrid]
-                for l in 1:approx[:nfunc]
+            for k in 1:approx.ngrid
+                for l in 1:approx.nfunc
                     α_temp = dgemm(1.0 * updated_approx_polynomials * bbtinv)
-                    α_new[(l - 1)*approx[:ngrid]+ k, j] = α_temp[l, k]
-                    α_new[(l - 1)*approx[:ngrid]+ k, approx[:ns] + j] = α_temp[approx[:nfunc] + l, k]
+                    α_new[(l - 1)*approx.ngrid+ k, j] = α_temp[l, k]
+                    α_new[(l - 1)*approx.ngrid+ k, approx.ns + j] = α_temp[approx.nfunc + l, k]
                 end
             end
 
@@ -141,7 +136,7 @@ function fixedpoint(m::GHLS, approx::SmolyakApproximation, α_initial::Array{Flo
         end
 
         # Normalize summed error to get average
-        avg_error /= 2*approx[:ngrid]*approx[:ns]
+        avg_error /= 2*approx.ngrid*approx.ns
 
         # Convergence fails if new α has non-numerical elements
         if (any([isnan(a) for a in α_new]) == true)
@@ -169,23 +164,23 @@ function simulate_linear(m::GHLS, approx::SmolyakApproximation)
     counter = 1
     convergence = true
 
-    statezlbinfo = zeros(Int64, approx[:ns])
-    endog_ergodmean = Array{Float64}(undef, approx[:nvars])
-    msvbounds = Array{Float64}(undef, 2*(approx[:nmsv] + approx[:nexogcont]))
-    shockindex = Array{Int64}(undef, approx[:nexog] - approx[:nexogcont])
+    statezlbinfo = zeros(Int64, approx.ns)
+    endog_ergodmean = Array{Float64}(undef, approx.nvars)
+    msvbounds = Array{Float64}(undef, 2*(approx.nmsv + approx.nexogcont))
+    shockindex = Array{Int64}(undef, approx.nexog - approx.nexogcont)
     countzlbstates = zeros(Int64, ns)
-    msvhigh = Array{Float64}(undef, approx[:nmsv], 1)
-    msvlow = Array{Float64}(undef, approx[:nmsv], 1)
-    innovations = Array{Float64}(undef, approx[:nexog], 1)
-    xrandn = Array{Float64}(undef, approx[:nexog], total_periods)
-    msv_std = zeros(approx[:nmsv] + approx[:nexogcont])
-    endogvar = zeros(approx[:nvars], total_periods+1)
+    msvhigh = Array{Float64}(undef, approx.nmsv, 1)
+    msvlow = Array{Float64}(undef, approx.nmsv, 1)
+    innovations = Array{Float64}(undef, approx.nexog, 1)
+    xrandn = Array{Float64}(undef, approx.nexog, total_periods)
+    msv_std = zeros(approx.nmsv + approx.nexogcont)
+    endogvar = zeros(approx.nvars, total_periods+1)
 
     # Set up random generator
     iseed = MersenneTwister(101294)
     randn!(iseed, xrandn)
     xrandnFortran=readdlm("xrandn.txt") #Don't have this file yet
-    xrandn=reshape(xrandnFortran,approx[:nexog],total_periods)
+    xrandn=reshape(xrandnFortran,approx.nexog,total_periods)
 
     # Set up for first period
     endogvar[1:nmsv,1]= m.steady_state[1:nmsv]
@@ -203,10 +198,10 @@ function simulate_linear(m::GHLS, approx::SmolyakApproximation)
         for ttsim in llim:ulim
 
             # Draw innovations
-            innovations[1:approx[:nexogshock]] = xrandn[1:approx[:nexogshock],ttsim-1]
+            innovations[1:approx.nexogshock] = xrandn[1:approx.nexogshock,ttsim-1]
 
-            if (approx[:nexogshock] > 0)
-                innovations[approx[:nexog]-approx[:nexogcont]+1:approx[:nexog]] = xrandn[approx[:nexog]-approx[:nexogcont]+1:approx[:nexog],ttsim-1]
+            if (approx.nexogshock > 0)
+                innovations[approx.nexog-approx.nexogcont+1:approx.nexog] = xrandn[approx.nexog-approx.nexogcont+1:approx.nexog,ttsim-1]
             end
 
             #THIS SHOULD BE DONE DIFFERENTLY HERE
@@ -219,19 +214,19 @@ function simulate_linear(m::GHLS, approx::SmolyakApproximation)
                 fill!(shockindex,1)
 
                 # Assigns each shock to position in grid
-                for i in 1:approx[:nexog]shock
+                for i in 1:approx.nexogshock
                     # If lower than lowest grid value assign lowest possible index
-                    if (endogvar[approx[:nvars]-approx[:nexog]+i,ttsim] < shockbounds[i,1])
+                    if (endogvar[approx.nvars-approx.nexog+i,ttsim] < shockbounds[i,1])
                         shockindex[i] = 1
 
                     # Otherwise assign it the index whose corresponding value is closest (rounding down)
                     else
-                        shockindex[i]  = min( nshockgrid[i], floor(1.0+(endogvar[approx[:nvars]-approx[:nexog]+i,ttsim]-shockbounds[i,1])/shockdistance[i]))
+                        shockindex[i]  = min( nshockgrid[i], floor(1.0+(endogvar[approx.nvars-approx.nexog+i,ttsim]-shockbounds[i,1])/shockdistance[i]))
                     end
                 end
 
                 # Indexes state that gave zlb
-                stateindex = exogposition(shockindex,nshockgrid,approx[:nexog]-approx[:nexog]cont)
+                stateindex = exogposition(shockindex,nshockgrid,approx.nexog-approx.nexogcont)
 
                 countzlbstates[stateindex] = countzlbstates[stateindex] + 1
                 if (countzlbstates[stateindex] > 5)
@@ -282,18 +277,18 @@ function simulate_linear(m::GHLS, approx::SmolyakApproximation)
     endog_emean = sum(endogvar[:,2:capt+1],dims=2)/capt
 
     # Calculate std for endogenous var
-    for i in 1:approx[:nmsv]
+    for i in 1:approx.nmsv
         msv_std[i] = sqrt(sum((endogvar[i,2:capt+1].-endog_emean[i]).^2 )/(capt-1))
     end
 
-    msvbounds[1:approx[:nmsv]] = endog_emean[1:approx[:nmsv]]-scalebd*msv_std
-    msvbounds[approx[:nmsvplus]+1:approx[:nmsvplus]+approx[:nmsv]] = endog_emean[1:approx[:nmsv]]+scalebd*msv_std
+    msvbounds[1:approx.nmsv] = endog_emean[1:approx.nmsv]-scalebd*msv_std
+    msvbounds[approx.nmsvplus+1:approx.nmsvplus+approx.nmsv] = endog_emean[1:approx.nmsv]+scalebd*msv_std
 
     # Calculate std for shocks
-    for i in 1:approx[:nexogcont]
-        msv_std[approx[:nmsv]+i] = sqrt(sum((endogvar[approx[:nvars]-i+1,2:capt+1] -endog_emean[approx[:nvars]-i+1]).^2 )/(capt-1))
-        msvbounds[approx[:nmsv]+i] = endog_emean[approx[:nvars]-i+1]-scalebd*msv_std[approx[:nmsv]+i]
-        msvbounds[approx[:nmsvplus]+approx[:nmsv]+counter] = endog_emean[approx[:nvars]-i+1]+scalebd*msv_std[approx[:nmsv]+counter]
+    for i in 1:approx.nexogcont
+        msv_std[approx.nmsv+i] = sqrt(sum((endogvar[approx.nvars-i+1,2:capt+1] -endog_emean[approx.nvars-i+1]).^2 )/(capt-1))
+        msvbounds[approx.nmsv+i] = endog_emean[approx.nvars-i+1]-scalebd*msv_std[approx.nmsv+i]
+        msvbounds[approx.nmsvplus+approx.nmsv+counter] = endog_emean[approx.nvars-i+1]+scalebd*msv_std[approx.nmsv+counter]
     end
 
     return endog_emean,zlbfrequency,msvbounds,statezlbinfo,convergence
@@ -303,57 +298,57 @@ end
 function initialalphas(m :: GHLS,approx::SmolyakApproximation,aalin::Array{Float64},bblin::Array{Float64})
 
     #Initilize variables
-    endogvar = Array{Float64}(undef,approx[:nvars])
-    exogpart = Array{Float64}(undef,approx[:nvars])
-    slopeconxxmsv = Array{Float64}(undef,2*approx[:nmsv])
-    slopeconcont = Array{Float64}(undef,2*approx[:nexogcont])
-    initialalphas = zeros(approx[:nfunc]*approx[:ngrid],2*ns)
-    alphass = zeros(approx[:nfunc],approx[:ngrid])
-    endogvarm1 = zeros(approx[:nvars],approx[:ngrid]) #holds lags
-    nmsvplus = approx[:nmsv] + approx[:nexogcont]
+    endogvar = Array{Float64}(undef,approx.nvars)
+    exogpart = Array{Float64}(undef,approx.nvars)
+    slopeconxxmsv = Array{Float64}(undef,2*approx.nmsv)
+    slopeconcont = Array{Float64}(undef,2*approx.nexogcont)
+    initialalphas = zeros(approx.nfunc*approx.ngrid,2*ns)
+    alphass = zeros(approx.nfunc,approx.ngrid)
+    endogvarm1 = zeros(approx.nvars,approx.ngrid) #holds lags
+    nmsvplus = approx.nmsv + approx.nexogcont
     endog
 
     # Get conversion from xx to msv
-    slopeconxxmsv[1:approx[:nmsv]] = approx.slopeconxx[1:approx[:nmsv]]
-    slopeconxxmsv[approx[:nmsv]+1:2*approx[:nmsv]] = approx.slopeconxx[nmsvplus+1:nmsvplus+approx[:nmsv]]
+    slopeconxxmsv[1:approx.nmsv] = approx.slopeconxx[1:approx.nmsv]
+    slopeconxxmsv[approx.nmsv+1:2*approx.nmsv] = approx.slopeconxx[nmsvplus+1:nmsvplus+approx.nmsv]
 
-    if (approx[:nexogcont] > 0)
-        slopeconcont[1:approx[:nexogcont]] = approx.slopeconxx[approx[:nmsv]+1:nmsvplus]
-        slopeconcont[approx[:nexogcont]+1:2*approx[:nexogcont]] = approx.slopeconxx[nmsvplus+approx[:nmsv]+1:2*nmsvplus]
+    if (approx.nexogcont > 0)
+        slopeconcont[1:approx.nexogcont] = approx.slopeconxx[approx.nmsv+1:nmsvplus]
+        slopeconcont[approx.nexogcont+1:2*approx.nexogcont] = approx.slopeconxx[nmsvplus+approx.nmsv+1:2*nmsvplus]
     end
 
     # Converts endog var back to msv domain
-    for i in 1:approx[:ngrid]
-        endogvarm1[1:approx[:nmsv],i] = msv2xx(xgrid[1:approx[:nmsv],i],approx[:nmsv],slopeconxxmsv)-m.steady_state[1:approx[:nmsv]] #CHECK THIS FUNCTION
+    for i in 1:approx.ngrid
+        endogvarm1[1:approx.nmsv,i] = msv2xx(xgrid[1:approx.nmsv,i],approx.nmsv,slopeconxxmsv)-m.steady_state[1:approx.nmsv] #CHECK THIS FUNCTION
     end
 
     # For each shock grid point
     for ss in 1:ns
-        yy = zeros(approx[:nfunc],approx[:ngrid])
-        exogval = zeros(approx[:nexog])
+        yy = zeros(approx.nfunc,approx.ngrid)
+        exogval = zeros(approx.nexog)
 
-        for i in 1:approx[:ngrid]
-            exogval[1:approx[:nexogshock]] = exoggrid[1:approx[:nexogshock],ss]   #update shocks (in deviation from ss)
+        for i in 1:approx.ngrid
+            exogval[1:approx.nexogshock] = exoggrid[1:approx.nexogshock,ss]   #update shocks (in deviation from ss)
 
-            if (approx[:nexogcont] > 0)
-                exogval[approx[:nexog]-approx[:nexogcont]+1:approx[:nexog]] = msv2xx(xgrid[approx[:nmsv]+1:approx[:nmsv]+approx[:nexogcont],i],approx[:nexogcont],slopeconcont)-endogsteady[approx[:nvars]+approx[:nexog]-approx[:nexogcont]+1:approx[:nvars]+approx[:nexog]]
+            if (approx.nexogcont > 0)
+                exogval[approx.nexog-approx.nexogcont+1:approx.nexog] = msv2xx(xgrid[approx.nmsv+1:approx.nmsv+approx.nexogcont,i],approx.nexogcont,slopeconcont)-endogsteady[approx.nvars+approx.nexog-approx.nexogcont+1:approx.nvars+approx.nexog]
             end
 
             #get linear solution
             endogvar=dgemv(1.0, aalin, endogvarm1[:,i]) #REMAKE THIS FUNCTION
             exogpart=dgemv(1.0, bblin, exogval) # REMAKE THIS FUNCTION
-            endogvar = endogsteady[1:approx[:nvars]] + endogvar + exogpart
+            endogvar = endogsteady[1:approx.nvars] + endogvar + exogpart
             yy[:,i] = endogvar[[10,11,18,19,21,22,13] ]
         end
 
         # Get alphas by inverting approximation function
-        alphass=dgemm(1.0,yy,approx[:bbtinv])
-        for i in 1:approx[:ngrid]
-            for ifunc in 1:approx[:nfunc]
+        alphass=dgemm(1.0,yy,approx.bbtinv)
+        for i in 1:approx.ngrid
+            for ifunc in 1:approx.nfunc
                 #if (alphass(ifunc,i) < 1.0e-8) alphass(ifunc,i) = 0.0d0
-                initialalphas[(ifunc-1)*approx[:ngrid]+i,ss] = alphass[ifunc,i]
+                initialalphas[(ifunc-1)*approx.ngrid+i,ss] = alphass[ifunc,i]
                 #initial guess for ZLB polynomials
-                initialalphas[(ifunc-1)*approx[:ngrid]+i,ns+ss] = alphass[ifunc,i]
+                initialalphas[(ifunc-1)*approx.ngrid+i,ns+ss] = alphass[ifunc,i]
             end
         end
     end
