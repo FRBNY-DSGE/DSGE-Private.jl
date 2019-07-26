36fb0414a01ce768560a27db0c8a8df4d1a8826e
Shlok Goyal <shlok.goyal@gmail.com>
2019-07-26 10:19:00 -0400

    Observables now transformed in filter and measurement uses scaled value.

--- src/estimate/filter.jl
+++ src/estimate/filter.jl
@@ -208,7 +208,7 @@ function filter_likelihood(m::GHLS, data::Matrix{S}, Φ::Function, Ψ::Function,
     lagged_variable_indices = [m.endogenous_states[:y_t], m.endogenous_states[:c_t], m.endogenous_states[:i_t]]
     s0 = append!(s0, s0[lagged_variable_indices])
     s0 = append!(s0, 0.0) #for innovation
-    s_init = initialize_state_draws(s0, F_ϵ, Φ,1000)# m[:n_particles].value)
+    s_init = initialize_state_draws(s0, F_ϵ, Φ, 1000)# m[:n_particles].value)
     println("tpf runs")
     # Run Tempered Particle filter, returns log-likelihoods
     loglh, cloglh, times = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u,
