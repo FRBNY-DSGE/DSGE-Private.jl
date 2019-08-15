c76588002f3ace2f1bc69fe4b2c0b0637751fe32
William Chen <wyc1@williams.edu>
2019-08-15 10:35:58 -0400

    Fix filepathing issue.

--- src/data/load_data.jl
+++ src/data/load_data.jl
@@ -88,13 +88,17 @@ function load_data(m::PoolModel{T}; cond_type::Symbol = :none,
     df = DataFrame()
     start_date = date_presample_start(m)
     end_data = date_mainsample_end(m)
+    savedir = dataroot(m)
+    if savedir[end] != "/"
+        savedir *= "/"
+    end
     for obs in values(m.observable_mappings)
         if isempty(df)
-            tmp_df = CSV.read(dataroot(m) * "raw/"* string(obs.input_series[1]) * ".csv")
+            tmp_df = CSV.read(savedir * "raw/"* string(obs.input_series[1]) * ".csv")
             df[:date] = Vector{Dates.Date}(tmp_df[:date])
             df[obs.key] = Vector{Float64}(tmp_df[obs.input_series[2]])
         else
-            tmp_df = CSV.read(dataroot(m) * "raw/" * string(obs.input_series[1]) * ".csv")
+            tmp_df = CSV.read(savedir * "raw/" * string(obs.input_series[1]) * ".csv")
             tmp_df[:date] = Vector{Dates.Date}(tmp_df[:date])
             tmp_df[obs.key] = Vector{Float64}(tmp_df[obs.input_series[2]])
             df = join(df, tmp_df[[:date, obs.key]], on = :date, kind = :outer)
