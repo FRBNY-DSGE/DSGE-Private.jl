9678c08c6c2dd2653f47b0928e674b63bfb23ffa
William Chen <wyc1@williams.edu>
2019-11-05 14:45:59 -0500

    Update AnSchorfheide to have one subspec which does a standard expoenntial transformation of model parameters and one that does the exponential used in the ss0 AnSchorfheide but with non-truncated value bounds.

--- src/models/an_schorfheide/subspecs.jl
+++ src/models/an_schorfheide/subspecs.jl
@@ -83,60 +83,60 @@ function ss2!(m::AnSchorfheide)
                    tex_label="e_R")
 end
 
-# Really experimental
+# Uses b = 1e5 for exponential transformation
 function ss3!(m::AnSchorfheide)
-    m <= parameter(:τ, 1.9937, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), GammaAlt(2., 0.5), fixed=false,
+    b = 1e9
+ m <= parameter(:τ, 1.9937, (0., Inf), (0., b), ModelConstructors.Exponential(), GammaAlt(2., 0.5), fixed=false,
                    description="τ: The inverse of the intemporal elasticity of substitution.",
                    tex_label="\\tau")
 
-    m <= parameter(:κ, 0.7306, (1e-20, 1e1), (1e-20, 1e1), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
+    m <= parameter(:κ, 0.7306, (0., 1.), (0., 1.), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
                    description="κ: Composite parameter in New Keynesian Phillips Curve.",
                    tex_label="\\kappa")
 
-    m <= parameter(:ψ_1, 1.1434, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), GammaAlt(1.5, 0.25), fixed=false,
+    m <= parameter(:ψ_1, 1.1434, (0., Inf), (0., b), ModelConstructors.Exponential(), GammaAlt(1.5, 0.25), fixed=false,
                    description="ψ_1: The weight on inflation in the monetary policy rule.",
                    tex_label="\\psi_1")
-    m <= parameter(:ψ_2, 0.4536, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), GammaAlt(0.5, 0.25), fixed=false,
+    m <= parameter(:ψ_2, 0.4536, (0., Inf), (0., b), ModelConstructors.Exponential(), GammaAlt(0.5, 0.25), fixed=false,
                    description="ψ_2: The weight on the output gap in the monetary policy rule.",
                    tex_label="\\psi_2")
 
-    m <= parameter(:rA, 0.0313, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), GammaAlt(0.5, 0.5), fixed=false,
+    m <= parameter(:rA, 0.0313, (0., Inf), (0., b), ModelConstructors.Exponential(), GammaAlt(0.5, 0.5), fixed=false,
                    description="rA: β (discount factor) = 1/(1+ rA/400).",
                    tex_label="rA")
 
-    m <= parameter(:π_star, 8.1508, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), GammaAlt(7., 2.), fixed=false,
+    m <= parameter(:π_star, 8.1508, (0., Inf), (0., b), ModelConstructors.Exponential(), GammaAlt(7., 2.), fixed=false,
                    description="π_star: Target inflation rate.",
                    tex_label="\\pi*")
 
-    m <= parameter(:γ_Q, 1.5, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), Normal(0.40, 0.20), fixed=false,
-
+    m <= parameter(:γ_Q, 1.5, (0., Inf), (0., b), ModelConstructors.Exponential(), Normal(0.40, 0.20), fixed=false,
                    description="γ_Q: Steady state growth rate of technology.",
                    tex_label="\\gamma_Q")
 
-    m <= parameter(:ρ_R, 0.3847, (1e-20, 1-1e-7), (1e-20, 1-1e-7), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
+    m <= parameter(:ρ_R, 0.3847, (0., 1.), (0., 1.), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
                    description="ρ_R: AR(1) coefficient on interest rate.",
                    tex_label="\\rho_R")
 
-    m <= parameter(:ρ_g, 0.3777, (1e-20, 1-1e-7), (1e-20, 1-1e-7), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
+    m <= parameter(:ρ_g, 0.3777, (0., 1.), (0., 1.), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
                    description="ρ_g: AR(1) coefficient on g_t = 1/(1 - ζ_t), where ζ_t is government spending as a fraction of output.",
                    tex_label="\\rho_g")
 
-    m <= parameter(:ρ_z, 0.9579, (1e-20, 1-1e-7), (1e-20, 1-1e-7), ModelConstructors.SquareRoot(), Uniform(0,1), fixed=false,
+    m <= parameter(:ρ_z, 0.9579, (0., 1.), (0., 1.), ModelConstructors.SquareRoot(), Uniform(0.,1.), fixed=false,
                    description="ρ_z: AR(1) coefficient on shocks to the technology growth rate.",
                    tex_label="\\rho_z")
 
-    m <= parameter(:σ_R, 0.4900, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), RootInverseGamma(4, .4), fixed=false,
+    m <= parameter(:σ_R, 0.4900, (0., Inf), (0., b), ModelConstructors.Exponential(), RootInverseGamma(4, .4), fixed=false,
                    description="σ_R: Standard deviation of shocks to the nominal interest rate.",
                    tex_label="\\sigma_R")
 
-    m <= parameter(:σ_g, 1.4594, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), RootInverseGamma(4, 1.), fixed=false,
+    m <= parameter(:σ_g, 1.4594, (0., Inf), (0., b), ModelConstructors.Exponential(), RootInverseGamma(4, 1.), fixed=false,
                    description="σ_g: Standard deviation of shocks to the government spending process.",
                    tex_label="\\sigma_g")
 
-    m <= parameter(:σ_z, 0.9247, (1e-20, 1e2), (1e-20, 1e5), ModelConstructors.Exponential(), RootInverseGamma(4, 0.5), fixed=false,
+    m <= parameter(:σ_z, 0.9247, (0., Inf), (0., b), ModelConstructors.Exponential(), RootInverseGamma(4, 0.5), fixed=false,
                    description="σ_z: Standard deviation of shocks to the technology growth rate process.",
                    tex_label="\\sigma_z")
-    m <= parameter(:e_y, 0.20*0.579923, fixed=true,
+  m <= parameter(:e_y, 0.20*0.579923, fixed=true,
                    description="e_y: Measurement error on GDP growth.",
                    tex_label="e_y")
 
