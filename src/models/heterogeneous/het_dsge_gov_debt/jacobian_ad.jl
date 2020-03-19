4694122667c20cfbbe6a645f017b11b92ca74b9e
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2020-03-19 15:19:11 -0400

    Allow autodiff to exclude functional derivatives

--- src/models/heterogeneous/het_dsge_gov_debt/jacobian_ad.jl
+++ src/models/heterogeneous/het_dsge_gov_debt/jacobian_ad.jl
@@ -1,4 +1,5 @@
-function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
+function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64};
+                     include_functional_eqs::Boolean = false)
     α      = m[:α].value::Float64
     δ      = m[:δ].value::Float64
     γ      = m[:γ].scaledvalue::Float64
@@ -49,11 +50,12 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
     ns   = DSGE.get_setting(m, :ns)::Int64
     nxns = nx*ns::Int64
 
-  @inline function mollifier(z, ehi, elo)
+    @inline function mollifier(z, ehi, elo)
         if z < ehi && z > elo
             return (2.0 / (ehi - elo)) *
                 exp(-1.0 / (1.0 - (-1.0 + 2.0 * (z - elo) /
                                    (ehi - elo))^2)) / 0.443993816237631
+
         end
         return 0.0
     end
@@ -80,7 +82,7 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
                                     t′_t::S, ω′_t::S, L′_t::S,
                                     cfunc::Vector{S},
                                     cfunc′::Vector{S}) where S<:Real
-    nxns = nx*ns
+        nxns = nx*ns
         ezt = exp(-z′_t)
         Rezt = (1+R_t) * ezt
         ωLt = ω′_t * L′_t
@@ -129,7 +131,7 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
                                        t′_t::S, w′_t::S,
                                        H′_t::Real, cfunc::Vector{S}) where S<:Real
 
-    nxns = nx*ns
+        nxns = nx*ns
         Rezt = (1+R_t)*(exp(-z′_t))
         wHt  = w′_t * H′_t
         # Loop over j
@@ -218,7 +220,7 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
         λ_w_t = x[1839:1839][1]
         λ_f_t = x[1840:1840][1]
         rm_t  = x[1841:1841][1]
-    l_t   = x[1842:2441]
+        l_t   = x[1842:2441]
 
         C_t   = x[2442:2442][1]
         R_t   = x[2443:2443][1]
@@ -243,7 +245,8 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
 
         #@show "Euler Eq."
         ell_euler_eq = Vector{Number}(undef, nxns)
-        ell_euler_eq = euler_equation(ell_euler_eq, R′_t, z′_t, t′_t, w′_t, L′_t, cfunc, cfunc′)
+        ell_euler_eq = euler_equation(ell_euler_eq, R′_t, z′_t, t′_t, w′_t,
+                                      L′_t, cfunc, cfunc′)
 
         #@show "KF"
         # KF Equation
@@ -257,14 +260,163 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
         eq_lambda          = margutil_t - (xswts .* kf_t)'*(1 ./ cfunc)
         eq_transfers       = capreturn_t * k_t * ezt - I_t + (1-mc_t) * y_t - tg_t - t_t
 
-   # Note: this beta is listed as βtil which differs from ̱β but Ethan thinks it
+        # Note: this beta is listed as βtil which differs from ̱β but Ethan thinks it
         # drops out when take derivative so doesnt matter
         eq_investment = margutil_t * Q_t * μ_t * (1- s_fn(I_t / I_t1 * ezt)) +
             β*margutil′_t * exp(-z′_t) * Q′_t * μ′_t * s_fn_prime(I′_t/I_t*exp(z′_t)) *
-            (I′_t/I_t*exp(z′_t))^2 - margutil_t * (1 + Q_t * μ_t*s_fn_prime(I_t/I_t1*exp(z_t)) *
-                                                 I_t/I_t1*exp(z_t))
+            (I′_t/I_t*exp(z′_t))^2 - margutil_t *
+            (1 + Q_t * μ_t*s_fn_prime(I_t/I_t1*exp(z_t)) * I_t/I_t1*exp(z_t))
 
-        eq_tobin_q = β * margutil′_t * exp(-z′_t) / margutil_t * (capreturn′_t + Q′_t * (1-δ))
+        eq_tobin_q = β * margutil′_t * exp(-z′_t) / margutil_t *
+            (capreturn′_t + Q′_t * (1-δ))
+
+        eq_capital_accumulation = k′_t - (1-δ) * k_t * ezt -
+            μ_t * (1 - s_fn(I_t/I_t1 * exp(z_t))) * I_t
+
+        # TODO: Figure out how to d\eal with kappa_w and kappa_p coefficients
+        ### Rows 1207 and 1208 LEAVE ALONE till hear from keshav
+        eq_wage_phillips =  1/(λ_w_t*Φ_w) * ((1+λ_w_t)*ϕ*L_t^ϕh - (1-τw)*margutil_t * w_t) +
+            β*b′_t*(π_w′_t/π_star_w - 1)*(π_w′_t/π_star_w)*(L′_t/L_t) -
+            (π_w_t/π_star_w - 1)*(π_w_t/π_star_w)
+
+        eq_price_phillips = -1/(λ_w_t*Φ_p)*((1+τF) - (1+λ_f_t)*mc_t) +
+            (1/(1 + R_t))*(π′_t/π_t -1)*(π′_t/π_star)*y′_t*exp(z′_t)*y_t -
+            (π_t/π_star -1)*(π_t/π_star) # see above
+
+        eq_marginal_cost = ((1-α)^(1-α)*α^α)^(-1)*w_t^(1-α)*capreturn_t^α - mc_t
+        eq_gdp           = exp(-α*z_t)*k_t^α*L_t^(1-α) - y_t
+        eq_optimal_kl    = α/(1-α)*L_t/k_t*ezt - capreturn_t/w_t
+        eq_taylor        = ((1+i_t) / (1+iii)) ^ ρ_R *
+            ((π / π_star) ^ ψπ * (y_t / y_t1 * exp(-γ)) ^ ψy) ^ (1-ρ_R) *
+            exp(rm′_t) - (1+i_t) / (1+iii)
+
+        # This is not equal because we're not log-linearizing the equations
+        eq_fisher = (1+R_t) - (1 + i_t) / π′_t
+
+        eq_nominal_wage_inflation = π_t * exp(z_t) * w_t / w_t1
+
+        eq_fiscal_rule = δb * (bg_t * ezt + (1 - 1/g_t) * y_t - bg / (1+R_t)) +
+            (1-δ)*Tg - tg_t
+
+        eq_g_budget_constraint = bg_t * ezt + g_t - tg_t - bg′_t * bg / (1+R_t)
+
+        eq_resource_constraint = y_t/g_t - C_t - I_t
+
+        eq_LI  = i′_t1 - i_t
+        eq_LY  = y′_t1 - y_t
+        eq_LW  = w′_t1 - w_t
+        eq_LX  = I′_t1 - I_t
+
+        # Add shock for this & below; doesn't matter b/c take derivative
+        eq_b   = b′_t - ρ_B * b_t
+        eq_g   = g′_t - ρ_G * g_t
+        eq_z   = z′_t - ρ_z * z_t
+        eq_μ   = μ′_t - ρ_μ * μ_t
+        eq_λ_w = λ_w′_t - ρ_lamw * λ_w_t
+        eq_λ_f = λ_f′_t - ρ_lamf * λ_f_t
+        eq_rm  = rm′_t  - ρ_mon * rm_t
+        return [eq_euler;
+                eq_kolmogorov_fwd;
+                eq_agg_consumption; eq_lambda; eq_transfers;
+                eq_investment; eq_tobin_q; eq_capital_accumulation; eq_wage_phillips;
+                eq_price_phillips; eq_marginal_cost; eq_gdp; eq_optimal_kl; eq_taylor;
+                eq_fisher;
+                eq_nominal_wage_inflation; eq_fiscal_rule; eq_g_budget_constraint;
+                eq_resource_constraint;
+                eq_LI; eq_LY; eq_LW; eq_LX;
+                eq_b; eq_g; eq_z; eq_μ; eq_λ_w; eq_λ_f; eq_rm]
+    end
+
+    function F_light(x::Vector{S}) where {S<:Real}
+
+        endo = DSGE.augment_model_states(m.endogenous_states_original,
+                                         DSGE.n_model_states_original(m))
+
+        kf′_t = x[1:600]
+
+        k′_t   = x[601:601][1]
+        i′_t1  = x[602:602][1]
+        y′_t1  = x[603:603][1]
+        w′_t1  = x[604:604][1]
+        I′_t1  = x[605:605][1]
+        bg′_t  = x[606:606][1]
+        b′_t   = x[607:607][1]
+        g′_t   = x[608:608][1]
+        z′_t   = x[609:609][1]
+        μ′_t   = x[610:610][1]
+        λ_w′_t = x[611:611][1]
+        λ_f′_t = x[612:612][1]
+        rm′_t  = x[613:613][1]
+
+        l′_t   = x[614:1213]
+
+        C′_t   = x[1214:1214][1]
+        R′_t   = x[1215:1215][1]
+        i′_t   = x[1216:1216][1]
+        t′_t   = x[1217:1217][1]
+        w′_t   = x[1218:1218][1]
+        L′_t   = x[1219:1219][1]
+        π′_t   = x[1220:1220][1]
+        π_w′_t = x[1221:1221][1]
+        margutil′_t = x[1222:1222][1]
+        y′_t   = x[1223:1223][1]
+        I′_t   = x[1224:1224][1]
+        mc′_t  = x[1225:1225][1]
+        Q′_t   = x[1226:1226][1]
+        capreturn′_t = x[1227:1227][1]
+        tg′_t  = x[1228:1228][1]
+
+        kf_t  = x[1229:1828]
+
+        k_t   = x[1829:1829][1]
+        i_t1  = x[1830:1830][1]
+        y_t1  = x[1831:1831][1]
+        w_t1  = x[1832:1832][1]
+        I_t1  = x[1833:1833][1]
+        bg_t  = x[1834:1834][1]
+        b_t   = x[1835:1835][1]
+        g_t   = x[1836:1836][1]
+        z_t   = x[1837:1837][1]
+        μ_t   = x[1838:1838][1]
+        λ_w_t = x[1839:1839][1]
+        λ_f_t = x[1840:1840][1]
+        rm_t  = x[1841:1841][1]
+        l_t   = x[1842:2441]
+
+        C_t   = x[2442:2442][1]
+        R_t   = x[2443:2443][1]
+        i_t   = x[2444:2444][1]
+        t_t   = x[2445:2445][1]
+        w_t   = x[2446:2446][1]
+        L_t   = x[2447:2447][1]
+        π_t   = x[2448:2448][1]
+        π_w_t = x[2449:2449][1]
+        margutil_t = x[2450:2450][1]
+        y_t   = x[2451:2451][1]
+        I_t   = x[2452:2452][1]
+        mc_t  = x[2453:2453][1]
+        Q_t   = x[2454:2454][1]
+        capreturn_t = x[2455:2455][1]
+        tg_t  = x[2456:2456][1]
+
+        cfunc′ = min.(1 ./ l′_t, xgrη)
+        cfunc  = min.(1 ./ l_t,  xgrη)
+
+        ezt = exp(-z_t)
+
+        eq_agg_consumption = (xswts .* kf_t)' * cfunc - C_t
+        eq_lambda          = margutil_t - (xswts .* kf_t)'*(1 ./ cfunc)
+        eq_transfers       = capreturn_t * k_t * ezt - I_t + (1-mc_t) * y_t - tg_t - t_t
+
+        # Note: this beta is listed as βtil which differs from ̱β but Ethan thinks it
+        # drops out when take derivative so doesnt matter
+        eq_investment = margutil_t * Q_t * μ_t * (1- s_fn(I_t / I_t1 * ezt)) +
+            β*margutil′_t * exp(-z′_t) * Q′_t * μ′_t * s_fn_prime(I′_t/I_t*exp(z′_t)) *
+            (I′_t/I_t*exp(z′_t))^2 - margutil_t *
+            (1 + Q_t * μ_t*s_fn_prime(I_t/I_t1*exp(z_t)) * I_t/I_t1*exp(z_t))
+
+        eq_tobin_q = β * margutil′_t * exp(-z′_t) / margutil_t *
+            (capreturn′_t + Q′_t * (1-δ))
 
         eq_capital_accumulation = k′_t - (1-δ) * k_t * ezt -
             μ_t * (1 - s_fn(I_t/I_t1 * exp(z_t))) * I_t
@@ -310,18 +462,21 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
         eq_λ_w = λ_w′_t - ρ_lamw * λ_w_t
         eq_λ_f = λ_f′_t - ρ_lamf * λ_f_t
         eq_rm  = rm′_t  - ρ_mon * rm_t
-      return [eq_euler;
-                eq_kolmogorov_fwd;
-                eq_agg_consumption; eq_lambda; eq_transfers;
+        return [eq_agg_consumption; eq_lambda; eq_transfers;
                 eq_investment; eq_tobin_q; eq_capital_accumulation; eq_wage_phillips;
-                eq_price_phillips; eq_marginal_cost; eq_gdp; eq_optimal_kl; eq_taylor; eq_fisher;
+                eq_price_phillips; eq_marginal_cost; eq_gdp; eq_optimal_kl;
+                eq_taylor; eq_fisher;
                 eq_nominal_wage_inflation; eq_fiscal_rule; eq_g_budget_constraint;
                 eq_resource_constraint;
                 eq_LI; eq_LY; eq_LW; eq_LX;
                 eq_b; eq_g; eq_z; eq_μ; eq_λ_w; eq_λ_f; eq_rm]
     end
 
-    JJ = ForwardDiff.jacobian(F, x)
+    JJ = if include_functional_eqs
+        ForwardDiff.jacobian(F, x)
+    else
+        vcat(functional_derivatives(x), ForwardDiff.jacobian(F_light, x))
+    end
     return JJ
 
 end
