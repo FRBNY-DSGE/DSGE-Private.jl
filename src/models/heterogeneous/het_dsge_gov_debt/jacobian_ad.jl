6629ee29a032651c09fe6106ee301235701e033f
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2020-03-19 18:46:48 -0400

    Fixing GDP eq

--- src/models/heterogeneous/het_dsge_gov_debt/jacobian_ad.jl
+++ src/models/heterogeneous/het_dsge_gov_debt/jacobian_ad.jl
@@ -435,7 +435,7 @@ function jacobian_ad(m::HetDSGEGovDebt, x::Vector{Float64})
             (π_t/π_star -1)*(π_t/π_star) # see above
 
         eq_marginal_cost = ((1-α)^(1-α)*α^α)^(-1)*w_t^(1-α)*capreturn_t^α - mc_t
-        eq_gdp           = exp(-α*z_t)*k_t^α*L_t^(1-α) - y_t
+        eq_gdp           = y_t - exp(-α*z_t)*k_t^α*L_t^(1-α)
 
         eq_optimal_kl    = capreturn_t/w_t - α/(1-α)*L_t/k_t*exp(z_t)
 
