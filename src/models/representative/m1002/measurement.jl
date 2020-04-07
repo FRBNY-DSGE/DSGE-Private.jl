17b918095584d578016124af62ab83a201961e3b
William Chen <chenwilliamy77@gmail.com>
2020-04-07 15:45:39 -0400

    Implement subspec tracking z_t, ztil_t, and zp_t in the measurement equation.

--- src/models/representative/m1002/measurement.jl
+++ src/models/representative/m1002/measurement.jl
@@ -208,6 +208,12 @@ function measurement(m::Model1002{T},
         # Add in wage markup as an additional observable
         ZZ[obs[:obs_ztilshock], endo[:ϵ_ztil_t]] = 1.
     end
+    if subspec(m) == "ss58"
+        # Add in wage markup as an additional observable
+        ZZ[obs[:obs_ztil], endo[:ztil_t]] = 1.
+        ZZ[obs[:obs_z], endo[:z_t]] = 1.
+        ZZ[obs[:obs_zp], endo[:zp_t]] = 1.
+    end
 
     return Measurement(ZZ, DD, QQ, EE)
 end
