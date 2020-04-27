c2ea22b63503f7f572272f9cce17be22fea4fcb7
William Chen <chenwilliamy77@gmail.com>
2020-04-27 17:56:59 -0400

    Implement and test full distribution forecasts with regime-switching.

--- src/forecast/drivers.jl
+++ src/forecast/drivers.jl
@@ -79,9 +79,9 @@ function prepare_forecast_inputs!(m::AbstractDSGEModel{S},
             @assert get_setting(m, :n_regimes) == length(get_setting(m, :regime_dates)) "The number" *
                 " in Setting :n_regimes ($(string(get_setting(m, :n_regimes))))" *
                 " does not match the length of Setting :regime_dates ($(string(length(get_setting(m, :regime_dates)))))."
-            @assert get_setting(m, :regime_dates)[1] == date_mainsample_start(m) "The first regime" *
-                " date ($(string(get_setting(m, :regime_dates)[1]))) must match the mainsample start date " *
-                "($(string(date_mainsample_start(m))))."
+            @assert get_setting(m, :regime_dates)[1] == date_presample_start(m) "The first regime" *
+                " date ($(string(get_setting(m, :regime_dates)[1]))) must match the presample start date " *
+                "($(string(date_presample_start(m))))."
         end
     end
 
@@ -362,7 +362,6 @@ function forecast_one(m::AbstractDSGEModel{Float64},
                       cond_obs_shocks::Dict{Symbol,Float64} = Dict{Symbol,Float64}(),
                       smooth_conditional::Symbol = :hist_cond,
                       cond_deviation_shocks::Vector{Symbol} = collect(keys(m.exogenous_shocks)),
-                      regime_switching::Bool = false, n_regimes::Int = 1,
                       bdd_fcast::Bool = true, params::Array{Float64} = Vector{Float64}(undef, 0))
 
     ### Common Setup
@@ -398,6 +397,12 @@ function forecast_one(m::AbstractDSGEModel{Float64},
     end
 
     output_dir = rawpath(m, "forecast")
+
+    # Determine if we are regime_switching
+    regime_switching = haskey(get_settings(m), :regime_switching) ?
+        get_setting(m, :regime_switching) : false
+    n_regimes        = regime_switching && haskey(get_settings(m), :n_regimes) ?
+        get_setting(m, :n_regimes) : 1
 
     # Print
     info_print(verbose, :low, "Forecasting input_type = $input_type, cond_type = $cond_type...")
@@ -484,8 +489,9 @@ function forecast_one(m::AbstractDSGEModel{Float64},
         end
 
         # Block info
-        block_inds, block_inds_thin = forecast_block_inds(m, input_type; subset_inds = subset_inds, params = params)
+        block_inds, block_inds_thin = forecast_block_inds(m, input_type; subset_inds = subset_inds)
         nblocks = length(block_inds)
+        @show block_inds
         start_block = forecast_start_block(m)
 
         # Info needed for printing progress
@@ -502,15 +508,19 @@ function forecast_one(m::AbstractDSGEModel{Float64},
                 params_for_map = load_draws(m, input_type, block_inds[block]; verbose = verbose)
             elseif input_type == :mode_draw_shocks
                 ndraws = length(block_inds[block])
-                params_for_map = repeat([params], ndraws)
+                @assert params <: Vector "To use mode_draw_shocks with params passed in as a keyword, params must be a Vector."
+                params_for_map = Vector{Float64}[params for i in block_inds[block]]
+            else
+                @show block_inds[block]
+                params_for_map = Vector{Float64}[params[i, :] for i in block_inds[block]]
             end
 
             mapfcn = use_parallel_workers(m) ? pmap : map
             if isempty(cond_obs_shocks)
-                forecast_outputs = mapfcn(param ->
+                forecast_outputs = mapfcn(para ->
                                           forecast_one_draw(m, input_type, cond_type,
                                                             output_vars,
-                                                            param, df, verbose = verbose,
+                                                            para, df, verbose = verbose,
                                                             use_filtered_shocks_in_shockdec =
                                                             use_filtered_shocks_in_shockdec,
                                                             shock_name = shock_name,
@@ -523,10 +533,10 @@ function forecast_one(m::AbstractDSGEModel{Float64},
                                                             n_regimes = n_regimes),
                                           params_for_map)
             else
-                forecast_outputs = mapfcn(param ->
+                forecast_outputs = mapfcn(para ->
                                           forecast_one_draw(m, input_type, cond_type,
                                                             output_vars,
-                                                            param, df, cond_obs_shocks,
+                                                            para, df, cond_obs_shocks,
                                                             verbose = verbose,
                                                             use_filtered_shocks_in_shockdec =
                                                             use_filtered_shocks_in_shockdec,
@@ -744,9 +754,12 @@ function forecast_one_draw(m::AbstractDSGEModel{Float64}, input_type::Symbol, co
         # Standardize shocks if desired
         if :histstdshocks in output_vars
             if regime_switching
-                start_date = max(date_presample_start(m), df[1, :date])
+                start_date = max(date_mainsample_start(m), df[1, :date]) # use mainsample b/c shocks only includes mainsample, no presample
                 end_date   = prev_quarter(date_forecast_start(m))
                 regime_inds = regime_indices(m, start_date, end_date)
+                if regime_inds[1][1] < 1
+                    regime_inds[1] = 1:regime_inds[1][end]
+                end
                 forecast_output[:histstdshocks] =
                     standardize_shocks(forecast_output[:histshocks], Matrix{eltype(system[1, :QQ])}[system[i, :QQ] for i in 1:n_regimes], regime_inds)
             else
