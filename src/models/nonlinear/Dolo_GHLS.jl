1980939a5c470179f1051279fe35c9b0a912349b
Shlok Goyal <shlok.goyal@gmail.com>
2020-09-28 09:23:07 -0400

    Add nonlinear model in Dolo

--- src/models/nonlinear/Dolo_GHLS.jl
+++ src/models/nonlinear/Dolo_GHLS.jl
@@ -2,4 +2,5 @@ using Dolo
 
 #model = Dolo.Model("~/.julia/dev/DSGE/src/models/nonlinear/GHLS.yaml")
 #model = Dolo.Model("~/.julia/dev/DSGE/src/models/nonlinear/model.yaml")
-model = yaml_import("ed_GHLS.yaml")
+#model = yaml_import("ed_GHLS.yaml")
+model = yaml_import("GHLS_final_2.yaml")
