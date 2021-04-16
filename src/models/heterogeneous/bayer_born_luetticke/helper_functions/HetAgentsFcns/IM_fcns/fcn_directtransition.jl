a7a5a2ec277eba7789b0d9ab0179dd473e5a7964
William Chen <chenwilliamy77@gmail.com>
2021-04-16 10:54:25 -0400

    Update DirectTransition to not use NumericalParameters

--- src/models/heterogeneous/bayer_born_luetticke/helper_functions/HetAgentsFcns/IM_fcns/fcn_directtransition.jl
+++ src/models/heterogeneous/bayer_born_luetticke/helper_functions/HetAgentsFcns/IM_fcns/fcn_directtransition.jl
@@ -4,21 +4,24 @@ function DirectTransition(m_a_star::Array,
     distr::Array,
     λ,
     Π::Array,
-    n_par::NumericalParameters)
+    grid_dims::NTuple{3, Int64},
+    k_grid::Vector, m_grid::Vector)
+
+    nm, nk, ny = grid_dims
 
     dPrime = zeros(eltype(distr),size(distr))
-    idk_a, wR_k_a = MakeWeightsLight(k_a_star,n_par.grid_k)
-    idm_a, wR_m_a = MakeWeightsLight(m_a_star,n_par.grid_m)
-    idm_n, wR_m_n = MakeWeightsLight(m_n_star,n_par.grid_m)
-    blockindex = (0:n_par.ny-1)*n_par.nk*n_par.nm
+    idk_a, wR_k_a = MakeWeightsLight(k_a_star,k_grid)
+    idm_a, wR_m_a = MakeWeightsLight(m_a_star,m_grid)
+    idm_n, wR_m_n = MakeWeightsLight(m_n_star,m_grid)
+    blockindex = (0:ny-1)*nk*nm
     @views @inbounds begin
-    for zz = 1:n_par.ny # all current income states
-        for kk = 1:n_par.nk # all current illiquid asset states
+    for zz = 1:ny # all current income states
+        for kk = 1:nk # all current illiquid asset states
             #idk_n = kk
-            for mm = 1:n_par.nm
+            for mm = 1:nm
                 dd=distr[mm,kk,zz]
-                IDD_a = (idm_a[mm,kk,zz].+(idk_a[mm,kk,zz] .-1).*n_par.nm)
-                IDD_n = (idm_n[mm,kk,zz].+(kk-1).*n_par.nm)
+                IDD_a = (idm_a[mm,kk,zz].+(idk_a[mm,kk,zz] .-1).*nm)
+                IDD_n = (idm_n[mm,kk,zz].+(kk-1).*nm)
                 dl    = (dd.*(1.0 .- wR_k_a[mm,kk,zz]))
                 DLL_a = (dl.*(1.0 .- wR_m_a[mm,kk,zz]))
                 DLR_a = (dl.*wR_m_a[mm,kk,zz])
@@ -28,14 +31,14 @@ function DirectTransition(m_a_star::Array,
                 DL_n  = (dd.*(1.0 .- wR_m_n[mm,kk,zz]))
                 DR_n  = (dd.*wR_m_n[mm,kk,zz])
                 pp    = (Π[zz,:])
-                for yy = 1:n_par.ny
+                for yy = 1:ny
                     id_a = IDD_a .+ blockindex[yy]
                     id_n = IDD_n .+ blockindex[yy]
                     fac = λ.*pp[yy]
                     dPrime[id_a]            += fac.*DLL_a
                     dPrime[id_a+1]          += fac.*DLR_a
-                    dPrime[id_a+n_par.nm]   += fac.*DRL_a
-                    dPrime[id_a+n_par.nm+1] += fac.*DRR_a
+                    dPrime[id_a+nm]   += fac.*DRL_a
+                    dPrime[id_a+nm+1] += fac.*DRR_a
                     dPrime[id_n]            += (1.0 .- λ).*pp[yy].*DL_n
                     dPrime[id_n+1]          += (1.0 .- λ).*pp[yy].*DR_n
                 end
