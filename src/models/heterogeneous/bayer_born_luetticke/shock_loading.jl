0ac599f30383f8c2a55699dec9dab501ef9e0fa1
William Chen <chenwilliamy77@gmail.com>
2021-05-11 10:39:09 -0400

    Refactor shock-loading to avoid unnecessary allocations

--- src/models/heterogeneous/bayer_born_luetticke/shock_loading.jl
+++ src/models/heterogeneous/bayer_born_luetticke/shock_loading.jl
@@ -1,12 +1,32 @@
-function shock_loading(m::BayerBornLuetticke{T}, TTT_jump::Matrix{T}) where {T <: Real}
+function shock_loading(m::BayerBornLuetticke{T}) where {T <: Real}
 
-    # Set up
-    exo      = m.exogenous_shocks
-    endo     = m.endogenous_states
     n_states = n_backward_looking_states(m)::Int
     n_exo_sh = n_shocks_exogenous(m)::Int
-    _RRR     = zeros(n_states, n_exo_sh)
+    _RRR     = zeros(T, n_states, n_exo_sh)
+
+    return _shock_loading!(m, _RRR)
+end
+
+function shock_loading(m::BayerBornLuetticke{T}, TTT_jump::Matrix{T}) where {T <: Real}
+
+    n_states = n_backward_looking_states(m)::Int
+    n_exo_sh = n_shocks_exogenous(m)::Int
     RRR      = Matrix{T}(undef, n_model_states(m)::Int, n_exo_sh)
+
+    # Populate RRR in place
+    _shock_loading!(m, view(RRR, 1:n_states, :))
+
+    # Map shocks to jumps
+    RRR[n_states+1:end, :] = TTT_jump * view(RRR, 1:n_states, :)
+
+    return RRR
+end
+
+function _shock_loading!(m::BayerBornLuetticke{T}, _RRR::AbstractMatrix) where {T <: Real}
+
+    # Set up
+    exo  = m.exogenous_shocks
+    endo = m.endogenous_states
 
     # Populate _RRR for the shocks
     _RRR[first(endo[:A′_t]), exo[:A_sh]] = 1.
@@ -19,9 +39,5 @@ function shock_loading(m::BayerBornLuetticke{T}, TTT_jump::Matrix{T}) where {T <
     _RRR[first(endo[:S_sh′_t]), exo[:S_sh]] = 1.
     _RRR[first(endo[:P_sh′_t]), exo[:P_sh]] = 1.
 
-    # Loading on states and jumps
-    RRR[1:n_states, :]     = _RRR
-    RRR[n_states+1:end, :] = TTT_jump * _RRR
-
-    return RRR
+    return _RRR
 end
