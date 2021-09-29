5b1d83812ecc2195d03786f4fba7fc21a3115846
aidan-cg <41960733+aidan-cg@users.noreply.github.com>
2021-09-29 11:12:05 -0400

    get rid of erroneous print statements

--- src/estimate/metropolis_hastings.jl
+++ src/estimate/metropolis_hastings.jl
@@ -183,8 +183,6 @@ function metropolis_hastings(proposal_dist::Distribution,
             end
 
             for (k, block_a) in enumerate(blocks_free)
-                println("block a")
-                println(block_a)
                 # Draw para_new from the proposal distribution
                 para_subset = para_old[block_a]
 
