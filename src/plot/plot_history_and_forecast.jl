b03ed21d1adb13999101b69689e8a5ecabf36a22
Shlok Goyal <shlok.goyal@gmail.com>
2021-12-03 22:52:48 -0500

    Allow new DataFrames version use

--- src/plot/plot_history_and_forecast.jl
+++ src/plot/plot_history_and_forecast.jl
@@ -214,7 +214,7 @@ histforecast
     # Concatenate MeansBands
     var, hist, forecast = hf.args
     combined = cat(hist, forecast)
-    dates = combined.means[:date]
+    dates = combined.means[!,:date]
 
     # Assign date ticks
     date_ticks = Base.filter(x -> start_date <= x <= end_date,    dates)
@@ -225,7 +225,7 @@ histforecast
 
     # Bands
     sort!(bands_pcts, rev = true) # s.t. non-transparent bands will be plotted correctly
-    inds = findall(start_date .<= combined.bands[var][:date] .<= end_date)
+    inds = findall(start_date .<= combined.bands[var][!,:date] .<= end_date)
 
 
 
