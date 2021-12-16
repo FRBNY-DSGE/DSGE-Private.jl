9e3bdd47bbd9684e8ba89e6f483450a920bc96ad
Shlok Goyal <shlok.goyal@gmail.com>
2021-12-16 16:46:39 -0500

    Revert r_e fix

--- src/models/representative/plt/eqcond.jl
+++ src/models/representative/plt/eqcond.jl
@@ -74,9 +74,8 @@ function eqcond(m::PLT, reg::Int) # do not edit these inputs
 
     ### Inflation
     Γ0[eq[:eq_r], endo[:r_t]] = 1.0
-    Γ1[eq[:eq_r], endo[:r_t]] = 1.0
+    Γ1[eq[:eq_r], endo[:r_t]] = m[:ρ_r]
     Ψ[eq[:eq_r], exo[:r_sh]]  = 1.0
-    C[endo[:r_t]] = m[:ρ_r]
 
     ### Policy (optimal under commitment with lam = 0)
     # Γ0[eq[:eq_pol], endo[:π_t]] = 1
