32ad63d17eabc4ea648433dabcf3a4bce478f2f0
aidan-cg <41960733+aidan-cg@users.noreply.github.com>
2022-10-17 11:20:07 -0400

    update plotting functions to correctly sort df

--- src/plot/plot_forecast_comparison.jl
+++ src/plot/plot_forecast_comparison.jl
@@ -164,6 +164,7 @@ function plot_forecast_comparison(m_old::AbstractDSGEModel, m_new::AbstractDSGEM
                                   kwargs...)
 	    if save_as_csv
    	        df_plot_data = df_plot_data[setdiff(names(df_plot_data), [:mean_history])]
+            sort!(df_plot_data, [:dates])
             rename!(df_plot_data, :mean_forecast => Symbol("mean_forecast_old"))
 	    end
 
@@ -178,6 +179,7 @@ function plot_forecast_comparison(m_old::AbstractDSGEModel, m_new::AbstractDSGEM
                 mkdir("blog_plot_data")
             end
             rename!(df_plot_data, :mean_forecast => Symbol("mean_forecast_new"))
+            sort!(df_plot_data, [:dates])
             CSV.write(string("blog_plot_data/", get_setting(m_new, :data_vintage),
                              "_", replace(replace(title, " " => "_"), "," => ""), "_", var,
                              join(string.(weights), "_"), ".csv"), df_plot_data)
@@ -269,6 +271,7 @@ function plot_forecast_comparison(m_old::AbstractDSGEModel, m_new::AbstractDSGEM
                 mkdir("blog_plot_data")
             end
             rename!(df_plot_data, :mean_forecast => Symbol("mean_forecast_new"))
+            select!(df_plot_data, :mean_forecast_old, :mean_history, :mean_forecast_new)
             CSV.write(string("blog_plot_data/", get_setting(m_new, :data_vintage),
                              "_", replace(replace(title, " " => "_"), "," => ""), "_", var,
                              "_", join(string.(weights), "_"), ".csv"), df_plot_data)
