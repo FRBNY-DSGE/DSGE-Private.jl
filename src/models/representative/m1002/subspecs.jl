6c36dcc325bd75b59e8bc76fd54f7448524a3b65
Ramya Nallamotu <ramya.nallamotu@gmail.com>
2023-09-08 11:10:37 -0400

    Add implementation of subspec 102 to m1002

--- src/models/representative/m1002/subspecs.jl
+++ src/models/representative/m1002/subspecs.jl
@@ -127,6 +127,8 @@ function init_subspec!(m::Model1002)
         return ss100!(m)
     elseif subspec(m) == "ss101"
         return ss101!(m)
+    elseif subspec(m) == "ss102"
+        return ss102!(m)
     else
         error("This subspec is not defined.")
     end
@@ -7099,4 +7101,8 @@ end
 # ss100 w/ estimated expected ZLB length in 2020Q4 ## not yet: estimated imperfect cred wts
 function ss101!(m)
     ss100!(m)
+end
+
+function ss102!(m)
+    ss97!(m) #but with different series (:PCE10) used instead of the usual :ASACX10 and without subtraction of 50 bps
 end
