389d99d406867ad3b00a538f7630d77e173f499a
Ibrahima Diagne <abediagne@gmail.com>
2025-05-08 13:09:54 -0400

    Made change to shock_decomp file to plot quarterly shock dec figures from briefing

--- src/forecast/shock_decompositions.jl
+++ src/forecast/shock_decompositions.jl
@@ -1112,10 +1112,10 @@ function shock_decompositions_quarters(m::AbstractDSGEModel,
         shock_qtrs = [1:end_ind] ## Defaults to regular shock dec
     end
 
-    shock_decompositions_quarters(system, horizon, histshocks, start_ind, end_ind, shock_qtrs = shock_qtrs, back_shocks = back_shocks)
+    shock_decompositions_quarters(m, system, horizon, histshocks, start_ind, end_ind, shock_qtrs = shock_qtrs, back_shocks = back_shocks)
 end
 
-function shock_decompositions_quarters(system::System{S},
+function shock_decompositions_quarters(m::AbstractDSGEModel, system::System{S},
     forecast_horizons::Int, histshocks::Matrix{S},
     start_index::Int, end_index::Int;
     shock_qtrs::Vector{UnitRange{Int}} = [1:end_index], back_shocks::Vector{Symbol} = Symbol[]) where {S<:AbstractFloat}
@@ -1159,7 +1159,8 @@ function shock_decompositions_quarters(system::System{S},
 
         for k in 1:length(shock_qtrs)
             st_ind = shock_qtrs[k]
-            for j in 1:(length(back_shocks) + size(states,4))
+            #for j in 1:(length(back_shocks) + size(states,4))
+            for j in 1:(length(back_shocks))
                 states3[:,i,(k-1)*shock_len+j+size(states,4)] = sum(states[st_ind,:,i,shock_inds[j]], dims = 1)
                 obs3[:,i,(k-1)*shock_len+j+size(states,4)] = sum(obs[st_ind,:,i,shock_inds[j]], dims = 1)
                 pseudo3[:,i,(k-1)*shock_len+j+size(states,4)] = sum(pseudo[st_ind,:,i,shock_inds[j]], dims = 1)
