7eb4882402bc9c0909acce7619d447dbb408dfe5
Brian Pacula <bpacula@san.rr.com>
2025-05-20 22:36:57 -0400

    Q2 short inflation continued

--- src/models/representative/m1002/measurement.jl
+++ src/models/representative/m1002/measurement.jl
@@ -242,13 +242,13 @@ idx_π1[1, endo[:π_t1]] = 1.
 
 
 TTT1_f   = view(TTT1Econo, endo[:π_t], :) ./ 4
-CCC1_f = ((2 .+ TTT[endo[:π_t], endo[:π_t]]) * CCC[endo[:π_t]]) ./ 4
-@show size(TTT1_f), size(CCC1_f)
+#CCC1_f = ((2 .+ TTT[endo[:π_t], endo[:π_t]]) * CCC[endo[:π_t]]) ./ 4
+#@show size(TTT1_f), size(CCC1_f)
 
 ZZ[obs[:obs_shortinflation], endo[:π_t1]] = 0.25
 ZZ[obs[:obs_shortinflation], endo[:π_t]] = 0.25
 ZZ[obs[:obs_shortinflation], :] .+= TTT1_f
-DD[obs[:obs_shortinflation]] = CCC1_f + 100*(m[:π_star]-1) #100*(m[:π_star]-1) + CCC1_f[endo[:π_t]]
+DD[obs[:obs_shortinflation]] = 100*(m[:π_star]-1) + (CCC1Econo[endo[:π_t]] * 0.25)
 
 #= #Implementation such that obs_shortinflation at time t is mean of t:t+3 of PseudoCorePCE (Rolling 4 quarter implementation)
     TTT1Econo, CCC1Econo = k_periods_ahead_expected_sums(TTT, CCC, TTTs, CCCs, reg, 3, 29;
