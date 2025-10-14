64bf390ce5d33a6324ac7c09e53628adef78202c
Elena Elbarmi <elenaelbarmi@gmail.com>
2025-10-14 14:37:18 -0400

    fixed param names

--- src/models/representative/DSSW/measurement.jl
+++ src/models/representative/DSSW/measurement.jl
@@ -34,10 +34,10 @@ function measurement(m::SmetsWouters{T},
     ZZ = zeros(_n_observables, _n_states)
     DD = zeros(_n_observables)
     EE = zeros(_n_observables, _n_observables)
-    QQ = zeros(_n_shocks_exogenous, _n_shocks_exogenous
+    QQ = zeros(_n_shocks_exogenous, _n_shocks_exogenous)
     
     _n_coint = 0
-    _n_lags = 0)
+    _n_lags = 0
 
     for para in m.parameters
         if !isempty(para.regimes)
@@ -55,19 +55,19 @@ function measurement(m::SmetsWouters{T},
     ZZ[obs[:obs_gdp], endo_addl[:y_t1]] = -1.0
     ZZ[obs[:obs_gdp], endo[:z_t]]       = 1.0
     #DD[obs[:obs_gdp]]                   = 100*(exp(m[:zstar])-1)
-    DD[obs[:obs_gdp]]                   = 100 * (m[:γ] + m[:α] * log(m[:υ])/(1-m[:α])) 
+    DD[obs[:obs_gdp]]                   = 100 * (m[:gam] + m[:alp] * log(m[:ups])/(1-m[:alp])) 
 
     ## Consumption Growth
     ZZ[obs[:obs_consumption], endo[:c_t]]       = 1.0
     ZZ[obs[:obs_consumption], endo_addl[:c_t1]] = -1.0
     ZZ[obs[:obs_consumption], endo[:z_t]]       = 1.0
-    DD[obs[:obs_consumption]]                   = 100*(m[:γ] + m[:α] * log(m[:υ])/(1-m[:α]))
+    DD[obs[:obs_consumption]]                   = 100*(m[:gam] + m[:alp] * log(m[:ups])/(1-m[:alp]))
 
     ## Investment Growth
     ZZ[obs[:obs_investment], endo[:i_t]]       = 1.0
     ZZ[obs[:obs_investment], endo_addl[:i_t1]] = -1.0
     ZZ[obs[:obs_investment], endo[:z_t]]       = 1.0
-    DD[obs[:obs_investment]]                   = 100*(m[:γ] + m[:α] * log(m[:υ])/(1-m[:α]))
+    DD[obs[:obs_investment]]                   = 100*(m[:gam] + m[:alp] * log(m[:ups])/(1-m[:alp]))
 
 
     ## Hours growth
@@ -78,11 +78,11 @@ function measurement(m::SmetsWouters{T},
     ZZ[obs[:obs_wages], endo[:w_t]]       = 1.0
     ZZ[obs[:obs_wages], endo_addl[:w_t1]] = -1.0
     ZZ[obs[:obs_wages], endo[:z_t]]       = 1.0
-    DD[obs[:obs_wages]]                   = 100*(m[:γ] + m[:α] * log(m[:υ])/(1-m[:α]))
+    DD[obs[:obs_wages]]                   = 100*(m[:gam] + m[:alp] * log(m[:ups])/(1-m[:alp]))
 
     ## Inflation (GDP Deflator)
-    ZZ[obs[:obs_gdpdeflator], endo[:π_star]]  = 1.0
-    DD[obs[:obs_gdpdeflator]]              = log(m[:π_star]
+    ZZ[obs[:obs_gdpdeflator], endo[:pistar]]  = 1.0
+    DD[obs[:obs_gdpdeflator]]              = log(m[:pistar])
 
     ## Nominal interest rate
     ZZ[obs[:obs_nominalrate], endo[:R_t]]       = 1.0
@@ -116,23 +116,25 @@ function measurement(m::SmetsWouters{T},
     EE[obs[:obs_consumption],6] = m[:e_c]^2
     EE[obs[:obs_investment],7] = m[:e_i]^2
 =#
-    EE[obs[:obs_gdp],obs[:obs_gdp]]=m[:e_y]^2
-    EE[obs[:obs_hours],obs[:obs_hours]]=m[:e_L]^2
-    EE[obs[:obs_wages],obs[:obs_wages]]=m[:e_w]^2
-    EE[obs[:obs_gdpdeflator],obs[:obs_gdpdeflator]]=m[:e_π]^2
-    EE[obs[:obs_nominalrate],obs[:obs_nominalrate]]=m[:e_R]^2
-    EE[obs[:obs_consumption],obs[:obs_consumption]]=m[:e_c]^2
-    EE[obs[:obs_investment],obs[:obs_investment]]=m[:e_i]^2
+
+    ## Measurement error variances (aligned with observables)
+
+    EE[obs[:obs_gdp],          obs[:obs_gdp]]          = m[:e_y]^2
+    EE[obs[:obs_hours],        obs[:obs_hours]]        = m[:e_L]^2
+    EE[obs[:obs_wages],        obs[:obs_wages]]        = m[:e_w]^2
+    EE[obs[:obs_gdpdeflator],  obs[:obs_gdpdeflator]]  = m[:e_pi]^2
+    EE[obs[:obs_nominalrate],  obs[:obs_nominalrate]]  = m[:e_R]^2
+    EE[obs[:obs_consumption],  obs[:obs_consumption]]  = m[:e_c]^2
+    EE[obs[:obs_investment],   obs[:obs_investment]]   = m[:e_i]^2
 
 
     #Variance of innovations
-    QQ[exo[:g_sh], exo[:g_sh]]           = m[:σ_g]^2
-    QQ[exo[:b_sh], exo[:b_sh]]           = m[:σ_b]^2
-    QQ[exo[:μ_sh], exo[:μ_sh]]           = m[:σ_μ]^2
-    QQ[exo[:z_sh], exo[:z_sh]]           = m[:σ_z]^2
-    QQ[exo[:λ_f_sh], exo[:λ_f_sh]]       = m[:σ_λ_f]^2
-    QQ[exo[:λ_w_sh], exo[:λ_w_sh]]       = m[:σ_λ_w]^2
-    QQ[exo[:rm_sh], exo[:rm_sh]]         = m[:σ_rm]^2
+    QQ[exo[:g_sh], exo[:g_sh]]           = m[:sig_g]^2
+    QQ[exo[:b_sh], exo[:b_sh]]           = m[:sig_b]^2
+    QQ[exo[:μ_sh], exo[:μ_sh]]           = m[:sig_mu]^2
+    QQ[exo[:z_sh], exo[:z_sh]]           = m[:sig_z]^2
+    QQ[exo[:laf_sh], exo[:laf_sh]]       = m[:sig_laf]^2
+    QQ[exo[:r_sh], exo[:r_sh]]         = m[:sig_rm]^2
 
     # These lines set the standard deviations for the anticipated
     # shocks to be equal to the standard deviation for the
@@ -141,9 +143,9 @@ function measurement(m::SmetsWouters{T},
         ZZ[obs[Symbol("obs_nominalrate$i")], :] = ZZ[obs[:obs_nominalrate], :]' * (TTT^i)
         DD[obs[Symbol("obs_nominalrate$i")]]    = m[:Rstarn]
         if subspec(m) == "ss1"
-            QQ[exo[Symbol("rm_shl$i")], exo[Symbol("rm_shl$i")]] = m[Symbol("σ_rm")]^2 / n_anticipated_shocks(m)
+            QQ[exo[Symbol("rm_shl$i")], exo[Symbol("rm_shl$i")]] = m[Symbol("sig_rm")]^2 / n_anticipated_shocks(m)
         else
-            QQ[exo[Symbol("rm_shl$i")], exo[Symbol("rm_shl$i")]] = m[Symbol("σ_rm")]^2 / 16
+            QQ[exo[Symbol("rm_shl$i")], exo[Symbol("rm_shl$i")]] = m[Symbol("sig_rm")]^2 / 16
         end
     end
 
