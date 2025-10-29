1360e902ef82931e2bf9e271c5e08717103783e8
Michael Vinh Pham <pham_michael@outlook.com>
2025-10-29 16:51:02 -0400

    still not working

--- src/estimate/lbfgs.jl
+++ src/estimate/lbfgs.jl
@@ -5,24 +5,58 @@ function lbfgs(fcn::Function,
                ftol::Float64        = 1e-14, # Default from csminwel
                grtol::Real          = 1e-8,  # default from Optim.jl
                iterations::Int      = 1000,
-               store_trace::Bool    = false,
-               show_trace::Bool     = false,
-               extended_trace::Bool = false,
+               store_trace::Bool    = true,
+               show_trace::Bool     = true,
+               extended_trace::Bool = true,
                verbose::Symbol      = :none,
                rng::AbstractRNG     = MersenneTwister(),
                autodiff::Bool       = false,
                kwargs...)
-    if autodiff
-        Optim.optimize(fcn, x0, LBFGS(m=15, linesearch = LineSearches.HagerZhang()),
-                       Optim.Options(g_tol = grtol, f_tol = ftol, x_tol = xtol,
-                                     iterations = iterations, store_trace = store_trace,
-                                     show_trace = show_trace,
-                                     extended_trace = extended_trace))
-    else
-        Optim.optimize(fcn, x0, LBFGS(m=15, linesearch = LineSearches.HagerZhang()), autodiff=:forward,
-                       Optim.Options(g_tol = grtol, f_tol = ftol, x_tol = xtol,
-                                     iterations = iterations, store_trace = store_trace,
-                                     show_trace = show_trace,
-                                     extended_trace = extended_trace))
+    
+    #callback time
+    iteration_times = Float64[]
+    start_time = time()
+
+    callback = function(state)
+        push!(iteration_times, time() - start_time)
+        false
     end
+
+
+    random_scaling = 0.97
+    x0 = x0 .* random_scaling
+
+    #wrapper objective fcn replace Inf with large finite value before gradients are computed
+    INF_REPLACEMENT = 1e15
+    fcn_wrapped = function(x)
+        val = fcn(x)
+        if isinf(val) || isnan(val) || val > INF_REPLACEMENT
+            return INF_REPLACEMENT
+        else
+            return val
+        end
+    end
+
+    #line search, high iters search
+    ls = LineSearches.BackTracking(order=2, maxstep=Inf, iterations=50)
+
+    result = if autodiff
+        Optim.optimize(fcn_wrapped, x0, LBFGS(m=10, linesearch = ls), autodiff=:forward,
+                       Optim.Options(g_tol = grtol, f_tol = ftol, x_tol = xtol,
+                                     iterations = iterations, store_trace = store_trace,
+                                     show_trace = show_trace,
+                                     extended_trace = extended_trace,
+                                     callback = callback,
+                                     allow_f_increases = true))
+    else
+        Optim.optimize(fcn_wrapped, x0, LBFGS(m=10, linesearch = ls),
+                       Optim.Options(g_tol = grtol, f_tol = ftol, x_tol = xtol,
+                                     iterations = iterations, store_trace = store_trace,
+                                     show_trace = show_trace,
+                                     extended_trace = extended_trace,
+                                     callback = callback,
+                                     allow_f_increases = true))
+    end
+
+    return result, iteration_times
 end
