270e7c29262f84e2af331fa84111f98fe13e1273
Michael Vinh Pham <pham_michael@outlook.com>
2025-11-07 16:15:34 -0500

    test file working

--- test/estimate/ss0_hessian.jl
+++ test/estimate/ss0_hessian.jl
@@ -33,7 +33,7 @@ if max_free_ind < maximum(para_free_inds)
     para_free_inds = para_free_inds[1:max_free_ind]
 end
 
-hessian, _ = hessian!(m, mode, data; verbose=:none)
+hessian, _ = hessian!(m, mode, data; hessian_method = :outer_product_gradient, verbose=:none)
 
 # The values here are liable to differ between machines, based on different
 # architectures/multithreading. Hessian values tend to be large, so these larger tolerances
