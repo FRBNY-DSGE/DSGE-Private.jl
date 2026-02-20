abe56b6e73c851e02539527f85dc428a61f19e9a
Ibrahima Diagne <abediagne@gmail.com>
2026-02-20 15:54:30 -0500

    Added custom dates to shock decomposition code + added consumption growth pseudo

--- src/plot/plot_shock_decomposition.jl
+++ src/plot/plot_shock_decomposition.jl
@@ -252,12 +252,14 @@ shockdec
                    legendfontsize = 6,
                    vert_line = quartertodate("0000-Q1"),
                    vert_line2 = quartertodate("0000-Q1"),
-                   trend_nostates = DataFrame(), df_enddate = Date(2100,12,31))
+                   trend_nostates = DataFrame(), df_enddate = Date(2100,12,31), custom_dates = [])
 
-    start_date = Date("2019-03-31")
-    end_date = Date("2024-06-30")
+    if !isempty(custom_dates)
+        start_date = custom_dates[1]
+        end_date = custom_dates[2]
+    end
+
     tick_size = 1
-
     # Error checking
     if length(sd.args) != 7 || typeof(sd.args[1]) != Symbol ||
         typeof(sd.args[2]) != MeansBands || typeof(sd.args[3]) != MeansBands ||
