a94c23ff882e89f6dce30818276a3c0f9a84fc96
gargparthb <gargparthb@gmail.com>
2026-06-24 15:01:33 -0400

    hetdsge tests

--- src/DSGE.jl
+++ src/DSGE.jl
@@ -429,14 +429,14 @@ module DSGE
     include("models/heterogeneous/real_bond_mkup/measurement.jl")
     include("models/heterogeneous/real_bond_mkup/augment_states.jl")
 
-    #=include("models/heterogeneous/het_dsge/het_dsge.jl")
+    include("models/heterogeneous/het_dsge/het_dsge.jl")
     include("models/heterogeneous/het_dsge/steady_state.jl")
     include("models/heterogeneous/het_dsge/subspecs.jl")
     include("models/heterogeneous/het_dsge/jacobian.jl")
     include("models/heterogeneous/het_dsge/shock_loading.jl")
     include("models/heterogeneous/het_dsge/observables.jl")
     include("models/heterogeneous/het_dsge/measurement.jl")
-    include("models/heterogeneous/het_dsge/augment_states.jl")=#
+    include("models/heterogeneous/het_dsge/augment_states.jl")
 
     include("models/heterogeneous/het_dsge_gov_debt/util.jl")
     include("models/heterogeneous/het_dsge_gov_debt/het_dsge_gov_debt.jl")
